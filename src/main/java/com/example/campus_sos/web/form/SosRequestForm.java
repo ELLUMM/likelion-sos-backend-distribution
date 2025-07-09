cb950e69c3f213953351bdd0cdb4750b565df98d
김재웅 <grbuguj@users.noreply.github.com>
2025-07-09 15:39:15 +0900

    fix: 폼 수 정

--- src/main/java/com/example/campus_sos/web/form/SosRequestForm.java
+++ src/main/java/com/example/campus_sos/web/form/SosRequestForm.java
@@ -4,6 +4,11 @@ import com.example.campus_sos.domain.sosrequest.BuildingType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+
+
 
 @Getter
 @Setter
@@ -13,4 +18,6 @@ public class SosRequestForm {
     private String title;
     private String content;
     private String openChatUrl;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
 }
