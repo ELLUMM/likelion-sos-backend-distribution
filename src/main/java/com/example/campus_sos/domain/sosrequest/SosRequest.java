07b7f267f080c05f1f6d2d8ea15c1536db1d8919
김재웅 <grbuguj@users.noreply.github.com>
2025-07-09 15:35:10 +0900

    fix: 배포환경맞게 수정

--- src/main/java/com/example/campus_sos/domain/sosrequest/SosRequest.java
+++ src/main/java/com/example/campus_sos/domain/sosrequest/SosRequest.java
@@ -4,6 +4,10 @@ import com.example.campus_sos.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
 
 @Entity
 @Setter
@@ -29,6 +33,10 @@ public class SosRequest {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member requester;
+    @CreationTimestamp // (대신 사용할 수도 있음)
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "helper_id")
